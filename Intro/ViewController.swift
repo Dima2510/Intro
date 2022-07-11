6d35c8d7f929c461a93f6c10c27c19179dc2c084
Dima <protsack.dima25@gmail.com>
2022-07-11 19:27:11 +0300

    Add Function

--- Intro/ViewController.swift
+++ Intro/ViewController.swift
@@ -9,11 +9,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var helloWorldLabel: UILabel!
+    @IBOutlet var toggleButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        helloWorldLabel.isHidden = true //label спрятан
+        toggleButton.layer.cornerRadius = 10 //скругление углоов
         // Do any additional setup after loading the view.
     }
-
+   
+    @IBAction func makeButtonAction() {
+        if helloWorldLabel.isHidden {
+            helloWorldLabel.isHidden = false
+            /// при нажатии и появлении лейбла меням название кнопки
+            toggleButton.setTitle("Hide Text", for: .normal)
+        } else {
+            ///если лейбл уже показан,то делаем все наоборот
+            helloWorldLabel.isHidden = true
+            toggleButton.setTitle("Show Text", for: .normal)
+        }
+    }
+    
 
 }
 
